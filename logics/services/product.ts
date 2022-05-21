d0bfc10653e2c5dc2e47da299db568bb479927cc
Kamran <kamran.davar.kd@gmail.com>
2022-05-21 16:18:31 +0430

    services and client created

---
+++ logics/services/product.ts
@@ -0,0 +1,15 @@
+import { fakestoreapiApi } from "../clients/fakestoreapi";
+
+import { product, products, id } from "../types";
+
+export const getProducts = async () => {
+  const response = await fakestoreapiApi.get<products>(
+    "/products"
+  );
+  console.log("response", response);
+  return response.data;
+};
+export const getSingleProduct = async (id: id) => {
+  const response = await fakestoreapiApi.get<product>(`/products/${id}`);
+  return response.data;
+};
