0857e498119efe5cb69d84db165b04fcb1a3d961
Kamran <kamran.davar.kd@gmail.com>
2022-05-21 16:23:42 +0430

    nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import useLoadingPage from "../logics/loading";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useLoadingPage();
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
