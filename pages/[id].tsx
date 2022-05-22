8bd5e53f3172eab6b8dac7a89c7c5a59e7a65eac
KamranDavar <kamran.davar.kd@gmail.com>
2022-05-23 00:12:01 +0430

    working on css

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -10,9 +10,7 @@ import { getSingleProduct } from "../logics/services/product";
 import { product, id } from "../logics/types";
 import { BsShare } from "react-icons/bs";
 import { IoMdCart } from "react-icons/io";
-import Rate from "rc-rate";
 import BeautyStars from "beauty-stars";
-import Rater from "react-rater";
 
 const ProductDetail: NextPage = ({
   product,
@@ -24,25 +22,22 @@ const ProductDetail: NextPage = ({
         <meta name="description" content={product.description} />
       </Head>
 
-      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-5xl m-6 lg:mt-48 ">
+      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-5xl m-6 lg:mt-36 ">
         <div className="md:flex">
           <div className="md:shrink-0">
             <img
-              className="h-auto w-full object-cover md:h-full md:w-72"
+              className="h-auto w-full md:h-full md:w-72"
               src={product.image}
               alt={product.title}
             />
           </div>
           <div className="p-8">
             <div className="md:flex">
-              <h1
-                className="block mt-1 flex-auto text-3xl text-black"
-              >
+              <h1 className="block mt-1 flex-auto text-3xl text-black">
                 {product.title}
               </h1>
               <div className="my-2 md:mt-4">
-
-              <BeautyStars value={3} size={12} activeColor="orange" />
+                <BeautyStars value={3} size={12} activeColor="orange" />
               </div>
             </div>
             <div className="text-lg mb-8 ">
@@ -55,13 +50,13 @@ const ProductDetail: NextPage = ({
             <h2>Description</h2>
             <p className="mt-2 text-slate-500">{product.description}</p>
             <div className="flex items-baseline my-8  border-b border-slate-200"></div>
-            <div className="flex space-x-4 mb-6 text-sm font-medium">
+            <div className="flex content-center space-x-4 mb-6 text-sm font-medium">
               <div className="flex-auto">
                 <button
-                  className=" px-6 py-4 shadow-xl w-fit font-semibold rounded-md bg-gradient-to-t from-red-500 to-orange-500 text-white text-lg"
+                  className=" px-6 py-4 shadow-xl w-fit font-semibold rounded-md bg-gradient-to-t from-red-700 to-red-500 text-white text-lg"
                   type="submit"
                 >
-                    <IoMdCart className="inline text-xl" /> ADD TO CART
+                  <IoMdCart className="inline text-xl" /> ADD TO CART
                 </button>
               </div>
               <button
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,27 +8,36 @@ import Image from "next/image";
 import Link from "next/link";
 import { getProducts } from "../logics/services/product";
 import { product } from "../logics/types";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = ({
   products,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
-    <div className={styles.container}>
+    <div className="px-4">
       <Head>
         <title>products list</title>
         <meta name="description" content="Beheading products list" />
       </Head>
       <main>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        {products.map((item: product, index: number) => (
-          <p key={index}>
-            {" "}
-            <Link href={`/${item.id}`}>
-              <a>{item.title}</a>
+        <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {products.map((item: product, index: number) => (
+            <Link key={index} href={`/${item.id}`} className="f">
+              <a className=" flex border border-slate-100 rounded-md lex flex-col items-center justify-center w-full max-w-lg mx-auto">
+                <img
+                  className=" rounded-md h-72 w-30 xl:h-80"
+                  src={item.image}
+                  alt="T-Shirt"
+                />
+                <div className="p-2">
+                  <h4 className="mt-2 text-lg font-medium text-gray-700 dark:text-gray-200">
+                    {item.title}
+                  </h4>
+                  <p className="text-blue-500 test-lg "> ${item.price}</p>
+                </div>
+              </a>
             </Link>
-          </p>
-        ))}
+          ))}
+        </div>
       </main>
     </div>
   );
